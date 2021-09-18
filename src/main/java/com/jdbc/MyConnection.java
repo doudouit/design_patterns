1b1feb1c298a8aa9088ac9c8f926b43531d425c2
窦建新 <180449@gree.com.cn>
2021-09-18 09:56:01 +0800

    手写jdbc优化-关闭连接

--- src/main/java/com/jdbc/MyConnection.java
+++ src/main/java/com/jdbc/MyConnection.java
@@ -14,17 +14,40 @@ public class MyConnection {
     private static final String USRENAME = "root";
     private static final String PASSWORD = "allen123";
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        // 1. 加载驱动
-        Class.forName(DRIVER);
-        // 2. 获得连接
-        Connection connection = DriverManager.getConnection(URL, USRENAME, PASSWORD);
-        // 3. 获取处理对象
-        Statement statement = connection.createStatement();
-        // 4. 执行sql语句
-        ResultSet resultSet = statement.executeQuery("select user_name,pass_word from user");
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString(1) + "\t" + resultSet.getString(2));
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            // 1. 加载驱动
+            Class.forName(DRIVER);
+            // 2. 获得连接
+            connection = DriverManager.getConnection(URL, USRENAME, PASSWORD);
+            // 3. 获取处理对象
+            statement = connection.createStatement();
+            // 4. 执行sql语句
+            ResultSet resultSet = statement.executeQuery("select user_name,pass_word from user");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1) + "\t" + resultSet.getString(2));
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // 5. 释放资源
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+            }
+
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+            }
         }
     }
 }
