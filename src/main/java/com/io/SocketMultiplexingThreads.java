6af6818926b176ac9af96ba60a3becb2b72b56b7
窦建新 <180449@gree.com.cn>
2021-11-29 11:47:21 +0800

    多路复用器多线程版

---
+++ src/main/java/com/io/SocketMultiplexingThreads.java
@@ -0,0 +1,185 @@
+package com.io;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @decription: 多路复用器多线程版
+ * @author: 180449
+ * @date 2021/11/29 9:07
+ */
+public class SocketMultiplexingThreads {
+
+    private ServerSocketChannel server = null;
+    private Selector selector1 = null;
+    private Selector selector2 = null;
+    private Selector selector3 = null;
+    private int port = 9090;
+
+
+    public static void main(String[] args) {
+        SocketMultiplexingThreads service = new SocketMultiplexingThreads();
+        service.init();
+        NioThread T1 = new NioThread(service.selector1, 2);
+        NioThread T2 = new NioThread(service.selector2);
+        NioThread T3 = new NioThread(service.selector3);
+
+        T1.start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        T2.start();
+        T3.start();
+
+        System.out.println("服务器启动了。。。。。");
+
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 初始化，将selector1注册到server上
+     */
+    public void init() {
+        try {
+            server = ServerSocketChannel.open();
+            server.configureBlocking(false);
+            server.bind(new InetSocketAddress(port));
+
+            selector1 = Selector.open();
+            selector2 = Selector.open();
+            selector3 = Selector.open();
+
+            server.register(selector1, SelectionKey.OP_ACCEPT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class NioThread extends Thread {
+    Selector selector = null;
+
+    int id = 0;
+    static AtomicInteger idx = new AtomicInteger();
+
+    static int selectors = 0;
+    volatile static BlockingQueue<SocketChannel>[] queue;
+
+    public NioThread(Selector selector) {
+        this.selector = selector;
+        id = idx.getAndIncrement() % selectors;
+        System.out.println("work:" + id + "已经启动");
+    }
+
+    public NioThread(Selector selector, int n) {
+        this.selector = selector;
+        selectors = n;
+        queue = new LinkedBlockingQueue[selectors];
+        for (int i = 0; i < n; i++) {
+            queue[i] = new LinkedBlockingQueue<>();
+        }
+        System.out.println("Boss 启动");
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                while (selector.select(10) > 0) {
+                    Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                    Iterator<SelectionKey> iterator = selectionKeys.iterator();
+
+                    while (iterator.hasNext()) {
+                        SelectionKey key = iterator.next();
+                        iterator.remove();
+                        if (key.isAcceptable()) {
+                            acceptHandler(key);
+                        } else if (key.isReadable()) {
+                            readHandler(key);
+                        }
+
+                    }
+
+                    // 给work用的
+                    if (!queue[id].isEmpty()) {
+                        ByteBuffer buffer = ByteBuffer.allocate(8192);
+                        SocketChannel client = queue[id].take();
+                        client.register(selector, SelectionKey.OP_READ, buffer);
+                        System.out.println("-------------------------------------------");
+                        System.out.println("新客户端：" + client.socket().getPort()+"分配到："+ (id));
+                        System.out.println("-------------------------------------------");
+                    }
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readHandler(SelectionKey key) {
+        SocketChannel client = (SocketChannel) key.channel();
+        ByteBuffer buffer = (ByteBuffer) key.attachment();
+        buffer.clear();
+        int read = 0;
+
+        try {
+            while (true) {
+                read = client.read(buffer);
+                if (read > 0) {
+                    buffer.flip();
+                    if (buffer.hasRemaining()) {
+                        client.write(buffer);
+                    }
+                    buffer.clear();
+                } else if (read == 0) {
+                    break;
+                } else {
+                    client.close();
+                    break;
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 把三次握手后的连接分配到自己queue中，等待work消费
+     * @param key
+     */
+    private void acceptHandler(SelectionKey key) {
+        try {
+            ServerSocketChannel server = (ServerSocketChannel) key.channel();
+            SocketChannel client = server.accept();
+            client.configureBlocking(false);
+
+            int num = idx.getAndIncrement() % selectors;
+
+            queue[num].add(client);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
